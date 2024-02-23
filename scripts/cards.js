9407dd5171deaf88187bebd64b0d59a8ba002b30
Chiri Francisco <franchiri00@gmail.com>
2024-02-23 16:08:12 -0300

    cambios

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,15 +1,18 @@
 const inicioBtn = document.getElementById('inicioBtn');
 const acercaBtn = document.getElementById('acercaBtn');
+const proyectosBtn = document.getElementById('proyectosBtn');
 const contactoBtn = document.getElementById('contactoBtn');
 
 const inicio = document.getElementById('intro');
 const acerca = document.getElementById('acerca');
+const proyectos = document.getElementById('proyectos');
 const contacto = document.getElementById('contacto');
 
 
 function mostrarInicio() {
     inicio.style.display = "block";
     acerca.style.display = "none";
+    proyectos.style.display = "none"
     contacto.style.display = "none";
 
     inicio.classList.add('entrada');
@@ -18,15 +21,27 @@ function mostrarInicio() {
 function mostrarAcerca() {
     inicio.style.display = "none";
     acerca.style.display = "block";
+    proyectos.style.display = "none"
     contacto.style.display = "none";
 
     acerca.classList.add('entrada');
     
 }
 
+function mostrarProyectos() {
+    inicio.style.display = "none";
+    acerca.style.display = "none";
+    proyectos.style.display = "block"
+    contacto.style.display = "none";
+
+    proyectos.classList.add('entrada');
+    
+}
+
 function mostrarContacto() {
     inicio.style.display = "none";
     acerca.style.display = "none";
+    proyectos.style.display = "none"
     contacto.style.display = "block";
 
     contacto.classList.add('entrada');
@@ -40,6 +55,12 @@ inicioBtn.addEventListener('click', () => {
 acercaBtn.addEventListener('click', () => {
     nav.classList.remove("navVisible")
     mostrarAcerca();
+})
+
+
+proyectosBtn.addEventListener('click', () => {
+    nav.classList.remove("navVisible")
+    mostrarProyectos();
 })
 
 contactoBtn.addEventListener('click', () => {
